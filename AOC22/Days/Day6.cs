7c4893d748f8972bf8343f992fa59db90b6ef716
enfy <enfy@rocketmail.com>
2022-12-07 22:54:29 +0100

    day6

--- AOC22/Days/Day6.cs
+++ AOC22/Days/Day6.cs
@@ -7,6 +7,24 @@ public class Day6 : IDailyPuzzle
 {
     public void Run(IEnumerable<string> inputLines)
     {
-        //Console.WriteLine("\nRunning Day 6:");
+        Console.WriteLine("\nRunning Day 6:");
+        var line = inputLines.First();
+        Console.WriteLine($"Part 1: {GetOffsetMarker(line, 4)}");
+        Console.WriteLine($"Part 2: {GetOffsetMarker(line, 14)}");
+    }
+
+    private int GetOffsetMarker(string line, int distinctCount)
+    {
+        var q = new Queue<char>(distinctCount);
+        for (var i = 0; i < distinctCount; i++) q.Enqueue(line[i]);
+        var offset = distinctCount;
+        while (offset < line.Length)
+        {
+            if (!q.GroupBy(x => x).Any(x => x.Count() > 1)) break;
+            q.Dequeue();
+            q.Enqueue(line[offset]);
+            offset++;
+        }
+        return offset;
     }
 }
---
+++ AOC22/Days/Day7.cs
@@ -0,0 +1,12 @@
+﻿using AOC22.Misc;
+
+namespace AOC22.Days;
+
+[Day(7)]
+public class Day7 : IDailyPuzzle
+{
+    public void Run(IEnumerable<string> inputLines)
+    {
+        //Console.WriteLine("\nRunning Day 7:");
+    }
+}
